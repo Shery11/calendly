55bf493f19ec173aa3ae500912b5ddbaf06a5342
Shery <shery_kirmani@hotmail.com>
2022-05-11 12:28:36 +0200

    readme md added

---
+++ readme.md
@@ -0,0 +1,32 @@
+# Foobar
+
+Demo calendly application
+
+## Installation
+
+Clone the repo
+
+### Client
+
+You should have angular cli installed run the following command to have the client side running.
+
+```bash
+ng serve  -o
+```
+
+### Server
+
+You should have nodejs installed run the following command
+
+```bash
+npm run build:serve
+```
+
+This will spin the express server in watch mode.
+
+### Tests
+
+Tests are written using jest and node version v14.15.3
+
+```bashnpm run test
+```
--- server/lib/index.js
+++ server/lib/index.js
@@ -1,13 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.server = exports.app = void 0;
 const admin = require("firebase-admin");
 const config_1 = require("./config");
 admin.initializeApp({
     credential: admin.credential.cert(config_1.APP_CONFIG.FIREBASE)
 });
 const api_1 = require("./api/api");
-api_1.default.listen(config_1.APP_CONFIG.PORT, () => {
+exports.app = api_1.default;
+const server = api_1.default.listen(config_1.APP_CONFIG.PORT, () => {
     console.log(`EXPRESS APP LISTENING ON PORT ${config_1.APP_CONFIG.PORT}`);
 });
-exports.default = api_1.default;
+exports.server = server;
 //# sourceMappingURL=index.js.map
